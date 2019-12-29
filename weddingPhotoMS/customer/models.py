ba46dbce9a433b533cda50647a44b2e71f59f1b0
jiang <18438622728@163.com>
2019-12-29 21:28:49 +0800

    完成添加订单功能

--- weddingPhotoMS/customer/models.py
+++ weddingPhotoMS/customer/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from myAdmin.models import Bridal_Veil as bv
+from myAdmin.models import Bridal_Veil
 from myAdmin.models import Bridal_Veils as bvs
 from cameraman.models import Cameraman
 from myAdmin.models import Space
@@ -20,18 +20,18 @@ class Customer(models.Model):
 # 创建订单表
 class Order(models.Model):
     id = models.AutoField(primary_key=True, verbose_name='订单主键')
-    order_nameOne = models.CharField(max_length=50,null=True,verbose_name='男方姓名')
-    order_nameTwo = models.CharField(max_length=50,null=True,verbose_name='女方姓名')
-    order_phoneNum = models.CharField(max_length=11,null=True,verbose_name='联系电话')
-    order_bv = models.ManyToManyField(to=bv, verbose_name='订单中的婚纱')
+    order_nameOne = models.CharField(max_length=50, null=True, verbose_name='男方姓名')
+    order_nameTwo = models.CharField(max_length=50, null=True, verbose_name='女方姓名')
+    order_phoneNum = models.CharField(max_length=11, null=True, verbose_name='联系电话')
+    order_bv = models.ManyToManyField(Bridal_Veil, verbose_name='订单中的婚纱')
     order_bvs = models.ForeignKey(to=bvs, to_field='id', null=True, on_delete=models.SET_NULL, verbose_name='婚纱组')
     order_photographTime = models.DateTimeField(verbose_name='拍照时间')
-    order_selectTime = models.DateTimeField(verbose_name='选片时间')
-    order_getTime = models.DateTimeField(verbose_name='取片时间')
+    order_selectTime = models.DateTimeField(null=True, verbose_name='选片时间')
+    order_getTime = models.DateTimeField(null=True, verbose_name='取片时间')
     order_space = models.ForeignKey(to=Space, to_field='id', null=True, on_delete=models.SET_NULL, verbose_name='场地')
     order_cameraman = models.ForeignKey(to=Cameraman, to_field='id', null=True, on_delete=models.SET_NULL,
                                         verbose_name='摄影师')
-    order_moneyNum = models.IntegerField(verbose_name='订单总价')
+    order_moneyNum = models.IntegerField(null=True, verbose_name='订单总价')
     order_starTime = models.DateTimeField(auto_now_add=True, verbose_name='订单开始时间')
-    order_endTime = models.DateTimeField(verbose_name='订单结束时间')
+    order_endTime = models.DateTimeField(null=True, verbose_name='订单结束时间')
     order_status = models.CharField(max_length=200, default='待拍摄', verbose_name='订单状态')
--- weddingPhotoMS/customer/views.py
+++ weddingPhotoMS/customer/views.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from io import BytesIO
 
 from django.shortcuts import render
@@ -12,6 +13,7 @@ from myAdmin.models import Space
 from myAdmin.models import Notice
 from cameraman.models import Cameraman
 from customer.models import Customer
+from customer.models import Order
 
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET, require_POST, require_http_methods, require_safe
@@ -29,12 +31,79 @@ def index(request):
 
 
 def addOrder(request):
+    allbv = bv.objects.all()
+    allbvs = bvs.objects.all()
+    space = Space.objects.all()
+    cameraman = Cameraman.objects.all()
     if request.method == 'GET':
-        allbv = bv.objects.all()
-        allbvs = bvs.objects.all()
-        space = Space.objects.all()
-        cameraman = Cameraman.objects.all()
         return render(request, 'customer/makeOrder.html',
-                      {'allBv': allbv, 'allBvs': allbvs, 'space': space, 'cameraman': cameraman})
+                      {'msg': '请认真填写以下内容！', 'allBv': allbv, 'allBvs': allbvs, 'space': space, 'cameraman': cameraman})
     else:
-        return render(request, 'customer/index.html')
+        # 男方姓名
+        nameOne = request.POST.get('nameOne').strip()
+        # 女方姓名
+        nameTwo = request.POST.get('nameTwo').strip()
+        # 客户电话
+        phoneNum = request.POST.get('phoneNum').strip()
+        # 预定拍照时间
+        myTime = request.POST.get('time').strip()
+        myTime = datetime.datetime.strptime(myTime, '%Y-%m-%d %H:%M:%S')
+        print(myTime, type(myTime))
+        # 婚纱 bv or婚纱组 bvs
+        inlineRadioOptions = request.POST.get('inlineRadioOptions').strip()
+        # 婚纱
+        bv1 = request.POST.get('select1').strip()
+        bv2 = request.POST.get('select2').strip()
+        bv3 = request.POST.get('select3').strip()
+        bv4 = request.POST.get('select4').strip()
+        # 婚纱组
+        myBvs = request.POST.get('bvs').strip()
+        myBvs = bvs.objects.get(pk=myBvs)
+        # 场地
+        mySpace = request.POST.get('space').strip()
+        mySpace = Space.objects.get(pk=mySpace)
+        # 摄影师
+        cameraMan = request.POST.get('cameraman').strip()
+        cameraMan = Cameraman.objects.get(pk=cameraMan)
+
+        # try:
+        #     s=order.order_starTime
+        #     print(s,type(s))
+        # except Exception as e:
+        #     print(e)
+        if inlineRadioOptions == 'bv':
+            order = Order.objects.create(order_nameOne=nameOne, order_nameTwo=nameTwo, order_phoneNum=phoneNum,
+                                         order_space=mySpace, order_cameraman=cameraMan, order_photographTime=myTime)
+            bvSet = bv.objects.filter(Q(pk=bv1) | Q(pk=bv2) | Q(pk=bv3) | Q(pk=bv4))
+            price = 0
+            for i in bvSet:
+                price += i.bv_price
+
+            # bvS = bvs.objects.create(bvs_name=name, bvs_prices=price)
+            order.order_bv.add(*bvSet)
+            order.order_moneyNum = price
+            try:
+                order.save()
+                return render(request, 'customer/makeOrder.html',
+                              {'msg': '添加订单成功！', 'allBv': allbv, 'allBvs': allbvs, 'space': space,
+                               'cameraman': cameraman})
+            except Exception as e:
+                print(e)
+                return render(request, 'customer/makeOrder.html',
+                              {'msg': '添加订单失败！', 'allBv': allbv, 'allBvs': allbvs, 'space': space,
+                               'cameraman': cameraman})
+        else:
+            order = Order(order_nameOne=nameOne, order_nameTwo=nameTwo, order_phoneNum=phoneNum,
+                          order_space=mySpace, order_cameraman=cameraMan, order_photographTime=myTime)
+            order.order_bvs = myBvs
+            order.order_moneyNum = myBvs.bvs_prices
+            try:
+                order.save()
+                return render(request, 'customer/makeOrder.html',
+                              {'msg': '添加订单成功！', 'allBv': allbv, 'allBvs': allbvs, 'space': space,
+                               'cameraman': cameraman})
+            except Exception as e:
+                print(e)
+                return render(request, 'customer/makeOrder.html',
+                              {'msg': '添加订单失败！', 'allBv': allbv, 'allBvs': allbvs, 'space': space,
+                               'cameraman': cameraman})
